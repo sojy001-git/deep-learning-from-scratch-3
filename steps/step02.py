80c0ccdee409569a29ec105047ec57eb9704570d
sojy001-git <sojy001@naver.com>
2025-10-03 21:03:33 +0900

    Update step02.py

--- steps/step02.py
+++ steps/step02.py
@@ -25,7 +25,7 @@ class Square(Function):
 x = Variable(np.array(10)) #x.data=10
 f = Square() #객체 생성
 y = f(x) #Square()(x)==f.__call__(x) 호출  ... call은 f(x)로 되는데 forward는 f.forwrd(~~)
-         #self=f, input=x, x'=x.data, y=f.forward(x.data)==f.forward(10)==100, output=Variable(100) 
+         #self=f, input=x, x'=x.data==10, y=f.forward(10)==100, output=Variable(100) 
          #y = Variable(100)
 print(type(y))
 print(y.data) #y.data = 100 
